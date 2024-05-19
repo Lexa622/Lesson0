9a93dacbc165559ef79986e4d30715f28828f214
Lexa622 <lexa622@gmail.com>
2024-05-19 21:44:01 +0200

    Add files via upload

---
+++ HomeWork.py
@@ -0,0 +1,4 @@
+completed_tasks = 12
+hours_spent = 1.5
+course_name = 'Python'
+print('Курс: ',course_name, ', всего задач: ', completed_tasks,', затрачено часов: ', hours_spent,', среднее время выполнения: ', completed_tasks * hours_spent, 'часов.')
